0b80fdfa0d04fa44bd18cf128fb7de6a5ac73b1e
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-25 11:18:53 -0700

    add scroll buttons on posters

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,9 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "./axios";
 import "./Row.css";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const rowRef = useRef(null);
+
+  let interval = null;
+
+  const startScrollingLeft = () => {
+    interval = setInterval(() => {
+      if (rowRef.current) {
+        rowRef.current.scrollLeft -= 5;
+      }
+    }, 50);
+  };
+
+  const startScrollingRight = () => {
+    interval = setInterval(() => {
+      if (rowRef.current) {
+        rowRef.current.scrollLeft += 5;
+      }
+    }, 50);
+  };
+
+  const stopScrolling = () => {
+    clearInterval(interval);
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -17,7 +40,25 @@ function Row({ title, fetchUrl }) {
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row_posters">
+      {title !== "Too scared to choose? Let us pick for you" && (
+        <>
+          <div
+            className="scroll-button left"
+            onMouseOver={startScrollingLeft}
+            onMouseOut={stopScrolling}
+          >
+            &#8249;
+          </div>
+          <div
+            className="scroll-button right"
+            onMouseOver={startScrollingRight}
+            onMouseOut={stopScrolling}
+          >
+            &#8250;
+          </div>
+        </>
+      )}
+      <div className="row_posters" ref={rowRef}>
         {movies.map((movie) => (
           <img
             key={movie.id}
