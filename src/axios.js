af91315a86a9862d9f9e8893a32bd52066545f81
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-11-01 16:03:49 -0700

    fix typo in axios config

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "<your-backend-url>";
+    : "https://frightflix.onrender.com";
 
 const instance = axios.create({
   baseURL,
